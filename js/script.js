9dcf0414af1e01c61b3061f44e3fdfa2aff554a7
Kacper Lechicki <klechicki@poczta.fm>
2022-12-13 11:30:26 +0100

    before js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const modal = document.querySelector('.modal-shadow');
+const info = document.querySelector('.info');
+const closeBtn = document.querySelector('.close');
+
+const showModal = () => {
+	modal.style.display = 'block';
+};
+
+const hideModal = () => {
+	modal.style.display = 'none';
+};
+
+info.addEventListener('click', showModal);
+close.addEventListener('click', hideModal);
