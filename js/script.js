a9d3d5f882756161e074e944ac8e8868f399d713
Kacper Lechicki <klechicki@poczta.fm>
2022-12-13 18:24:30 +0100

    show timelist

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 const modal = document.querySelector('.modal-shadow');
 const info = document.querySelector('.info');
 const closeBtn = document.querySelector('.close');
+const timeList = document.querySelector('.time-list');
+const archiveBtn = document.querySelector('.history');
 
 const showModal = () => {
 	modal.style.display = 'block';
@@ -14,5 +16,14 @@ const hideModal = () => {
 	modal.classList.remove('modal-animation');
 };
 
+const handleTimeList = () => {
+	if (timeList.style.display === 'none') {
+		timeList.style.display = 'block';
+	} else {
+		timeList.style.display = 'none';
+	}
+};
+
 info.addEventListener('click', showModal);
 closeBtn.addEventListener('click', hideModal);
+archiveBtn.addEventListener('click', handleTimeList);
